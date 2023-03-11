ee1a5b45d31c9b052e59aa00405c6c915485ae9e
unknown <meus13@gmail.com>
2023-03-11 23:07:36 +0100

    Dodatkowe informajce

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    println!("to jest traktowane jako makro")
+    println!("to jest traktowane jako makro");
+
+    let mut x :i64 = 15;
+    x=x+2;
+    println!("csd{} cos {{}}" ,x);
 }
--- hello-rust/target/debug/deps/hello_rust-701db4de9b40bf53.d
+++ hello-rust/target/debug/deps/hello_rust-701db4de9b40bf53.d
@@ -1,5 +1,5 @@
-c:\Users\Menium\Desktop\rust-projekty\rust_podstawy\hello-rust\target\debug\deps\hello_rust-701db4de9b40bf53.rmeta: src\main.rs
+C:\Users\Menium\Desktop\rust-projekty\rust_podstawy\hello-rust\target\debug\deps\hello_rust-701db4de9b40bf53.rmeta: src\main.rs
 
-c:\Users\Menium\Desktop\rust-projekty\rust_podstawy\hello-rust\target\debug\deps\hello_rust-701db4de9b40bf53.d: src\main.rs
+C:\Users\Menium\Desktop\rust-projekty\rust_podstawy\hello-rust\target\debug\deps\hello_rust-701db4de9b40bf53.d: src\main.rs
 
 src\main.rs:
