6a19e825dd99663d47711137c633f17adea0b608
unknown <meus13@gmail.com>
2023-03-12 22:08:10 +0100

    Tuple ciekawe rozwiazania

---
+++ primitives-podstawowezmienne/target/debug/deps/primitives_podstawowezmienne-531895451d01132d.d
@@ -0,0 +1,8 @@
+c:\Users\Menium\Desktop\rust-projekty\rust_podstawy\primitives-podstawowezmienne\target\debug\deps\primitives_podstawowezmienne-531895451d01132d.rmeta: src\main.rs Cargo.toml
+
+c:\Users\Menium\Desktop\rust-projekty\rust_podstawy\primitives-podstawowezmienne\target\debug\deps\primitives_podstawowezmienne-531895451d01132d.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
---
+++ primitives-podstawowezmienne/target/debug/deps/primitives_podstawowezmienne-a8a72cfcd3bf69ba.d
@@ -0,0 +1,8 @@
+c:\Users\Menium\Desktop\rust-projekty\rust_podstawy\primitives-podstawowezmienne\target\debug\deps\primitives_podstawowezmienne-a8a72cfcd3bf69ba.rmeta: src\main.rs Cargo.toml
+
+c:\Users\Menium\Desktop\rust-projekty\rust_podstawy\primitives-podstawowezmienne\target\debug\deps\primitives_podstawowezmienne-a8a72cfcd3bf69ba.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
