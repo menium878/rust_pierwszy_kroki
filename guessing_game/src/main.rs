5ebda665a8f65fe8391e9e3b9f909d2c3e730760
unknown <meus13@gmail.com>
2023-03-25 22:59:45 +0100

    input uzytkownika

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
 
     let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Nie udało się przeczytać inputu");
+    println!("Zgadłeś {}",guess);
 
 }
