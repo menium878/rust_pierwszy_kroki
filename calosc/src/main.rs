cf27e63a0ed292f2915e563126b6008223119918
unknown <meus13@gmail.com>
2023-03-25 23:42:48 +0100

    Zmienne rozmiary

--- calosc/src/main.rs
+++ calosc/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         .expect("Nie otrzymałem inputu"); //Return Result więc musimy handle if fail
 
     println!("Hello, {}! {}",imie.trim_end(),powitanie); //trim_end do niezczytywania nowej lini usunięcie białego znaku końcówka
-*/
+
     const zmienna:u32=1_000_000;
     const PI:f32 =3.141592;
     let age="47" //string ""
@@ -24,4 +24,11 @@ fn main() {
         .expect("Age wasn't assigned a number");
     age = age +1;
     println!("I'm {} and I want ${}",age,zmienna)
+ */
+
+    //Unsigned int: u8, u16, u32, u64, u128, unsize
+    //Signed integer: i8, i16, i32, i64, i128, isize
+
+    println!("Max u32 {}",u32::MAX); // Ile zajmuje
+    println!("Max i32 {}",i32::MAX); 
 }
