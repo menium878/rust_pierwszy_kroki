195acf05f3e09c8492159d5cf2b8cf0936e0bf04
unknown <meus13@gmail.com>
2023-04-02 15:18:13 +0200

    Box

--- calosc/src/main.rs
+++ calosc/src/main.rs
@@ -439,5 +439,30 @@ fn main() {
 
     // !smart pointers
     // !&refrence
-        
+
+
+    // BOX - stores on heep not on stack czyli prawdopodobnie jest szybszy
+    // let b_int1 = Box::new(10);
+    // println!("b_int1={}",b_int1);
+    struct TreeNode<T>{
+       pub left:Option<Box<TreeNode<T>>>,
+       pub right:Option<Box<TreeNode<T>>>,
+       pub key: T, 
+    }
+    impl<T> TreeNode<T> {
+        pub fn new(key:T) -> Self{
+            TreeNode { left: None, right: None, key,}
+        }
+        pub fn left(mut self,node:TreeNode<T>)->
+        Self{
+            self.left=Some(Box::new(node));
+            self
+        }
+        pub fn right(mut self,node:TreeNode<T>)->
+        Self{
+            self.left=Some(Box::new(node));
+            self
+        }
+    }
+    let node1 = TreeNode::new(1).left(TreeNode::new(2)).right(TreeNode::new(5));
 }
